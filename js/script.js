6ff1ea6838b554a7d77a82bd25e79c3a0e84219a
André Queiroz <alqlima@gmail.com>
2020-10-15 10:41:52 -0300

    Displaying the error

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ let validator = {
 
         let inputs = form.querySelectorAll('input');
 
+        validator.clearErrors();
+
         for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
             let check = validator.checkInput(input);
             if (check !== true) {
                 send = false;
-                // exibir o erro
-                console.log(check);
+                validator.showError(input, check);
             }
         }
         //send = false;
@@ -39,6 +40,25 @@ let validator = {
             }
         }
         return true;
+    },
+    showError: (input, error) => {
+        input.style.borderColor = '#ff0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors: () => {
+        let inputs = form.querySelectorAll('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
+        let errorElements = document.querySelectorAll('.error');
+        for (let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
     }
 };
 
